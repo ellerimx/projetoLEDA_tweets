920485125abac0b9307a8c97748b0b7b36b8bc98
mirelle casimiro <mirellecs07@gmail.com>
2025-06-13 02:17:23 -0300

    organizando estrutura do projeto

--- src/processarCSV/EscreverCSV.java
+++ src/processarCSV/EscreverCSV.java
@@ -1,5 +1,106 @@
 package processarCSV;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import formatarTweet.Tweet;
+
 public class EscreverCSV {
+
+    // definindo o cminho do diretorio onde esta  arquivo tweets.csv
+    public static final String DIR_TWEETS_DATABASE = "C:\\Users\\Millena\\Documents\\mirelle\\LEDA\\projetoLEDA_tweets\\src\\dataBaseTweets";
+    public static final String DIR_PROJECT_DATABASE = System.getProperty("user.dir") + File.separator + "src" + File.separator + "dataBaseTweets";
+
+    public static void write_date_file(String fileName, Tweet[] data) {
+        String path = DIR_PROJECT_DATABASE + File.separator + fileName + ".csv";
+
+        try (BufferedWriter file = new BufferedWriter(new FileWriter(path))) {
+            System.out.println("Salvando data formatada...");
+            file.write("Target,ID,Date,flag,User,Text");
+            file.newLine();
+
+            for (Tweet tweet : data) {
+                String line = String.format("%s,%s,%s,%s,%s,%s",
+                        tweet.getTarget(),
+                        tweet.getId(),
+                        tweet.getFormatted_date(),
+                        tweet.getFlag(),
+                        tweet.getUser(),
+                        tweet.getText());
+                file.write(line);
+                file.newLine();
+            }
+            System.out.println("arquivo com datas transformadas salvo!\n");
+            
+        } catch (IOException e) {
+            System.out.println("Erro ao escrever arquivo");
+            e.printStackTrace();
+        }
+    }
+
+    public static void write_mentioned_persons_file(String fileName, Tweet[] data) {
+        String pathFormattedDate = DIR_PROJECT_DATABASE + File.separator + "tweets_formated_data.csv";
+        String pathMentionedPersons = DIR_PROJECT_DATABASE + File.separator + fileName + ".csv";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(pathFormattedDate));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(pathMentionedPersons))) {
+
+            System.out.println("Salvando arquivo das datas com menções...");
+
+            String line;
+            int i = 0;
+            while ((line = reader.readLine()) != null) {
+                if (i == 0) {
+                    writer.write(line + ",mentioned_person,mentioned_person_count");
+                } else {
+                    if (i - 1 < data.length && data[i - 1] != null) {
+                        writer.write(line + "," +
+                                data[i - 1].getMentioned_person() + "," +
+                                data[i - 1].getMentioned_person_count());
+                    }
+                }
+                writer.newLine();
+                i++;
+            }
+            System.out.println("arquivo com datas transformadas salvo!\n");
+
+        } catch (IOException e) {
+            System.out.println("Erro ao processar arquivo.");
+            e.printStackTrace();
+        }
+        
+    }
+
+    public static void write_ordened_file(String name, Tweet[] data) {
+        String path = DIR_PROJECT_DATABASE + File.separator + "dataBase_ordenada" + File.separator + name + ".csv";
+
+        try (BufferedWriter file = new BufferedWriter(new FileWriter(path))) {
+            file.write("Target,ID,Date,flag,User,Text,mentioned_person,mentioned_person_count");
+            file.newLine();
+
+            for (Tweet tweet : data) {
+                if (tweet != null) {
+                    String line = String.format("%s,%s,%s,%s,%s,%s,%s,%d",
+                            tweet.getTarget(),
+                            tweet.getId(),
+                            tweet.getFormatted_date(),
+                            tweet.getFlag(),
+                            tweet.getUser(),
+                            tweet.getText(),
+                            tweet.getMentioned_person(),
+                            tweet.getMentioned_person_count());
+                    file.write(line);
+                    file.newLine();
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao escrever arquivo");
+            e.printStackTrace();
+        }
+    }
     
 }
--- src/processarCSV/LeituraCSV.java
+++ src/processarCSV/LeituraCSV.java
@@ -1,5 +1,85 @@
 package processarCSV;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import estrutura_dados.Pilha;
+import formatarTweet.Tweet;
+
 public class LeituraCSV {
+
+    // definindo o cminho do diretorio onde esta  arquivo tweets.csv
+    public static final String DIR_TWEETS_DATABASE = "C:\\Users\\Millena\\Documents\\mirelle\\LEDA\\projetoLEDA_tweets\\src\\dataBaseTweets";
+    public static final String DIR_PROJECT_DATABASE = System.getProperty("user.dir") + File.separator + "src" + File.separator + "dataBaseTweets";
+
+    // definindo o tamanho do arquivo tweets
+    public static final int DATABASE_LENGTH = 1048575;
+
+     public static Tweet[] extract_tweets_database(String dir) {
+        String path = dir + File.separator + "tweets.csv";
+        Tweet[] database = new Tweet[DATABASE_LENGTH];
+        Pilha pilha = new Pilha(DATABASE_LENGTH);
+
+        try (BufferedReader file = new BufferedReader(new FileReader(path))) {
+            System.out.println("Extraindo database...");
+            String line;
+            int i = 0;
+            while ((line = file.readLine()) != null) {
+                if (i > 0) {
+                    String[] field = line.split(",");
+                    Tweet tweet = new Tweet(field[0], field[1], field[2], field[3], field[4], field[5].trim());
+                    pilha.empilhar(tweet);
+                }
+                i++;
+            }
+            System.out.println("Data base completa.");
+
+            for (int j = DATABASE_LENGTH - 1; j >= 0; j--) {
+                database[j] = pilha.desempilhar();
+            }
+        } catch (IOException e) {
+            System.out.println("Erro extraindo os dados");
+            e.printStackTrace();
+        }
+        return database;
+    }
+
+    public static Tweet[] extract_database(String name) {
+        String path = DIR_PROJECT_DATABASE + File.separator + name + ".csv";
+        Tweet[] database = new Tweet[DATABASE_LENGTH];
+        Pilha pilha = new Pilha(DATABASE_LENGTH);
+
+        try (BufferedReader file = new BufferedReader(new FileReader(path))) {
+            String line;
+            int i = 0;
+            while ((line = file.readLine()) != null) {
+                if (i > 0) {
+                    String[] field = line.split(",");
+                    int countMentionedPerson = 0;
+                    try {
+                        countMentionedPerson = Integer.parseInt(field[7]);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Erro ao converter números");
+                        e.printStackTrace();
+                    }
+                    Tweet tweet = new Tweet(field[0], field[1], field[2], field[3], field[4], field[5], field[6], countMentionedPerson);
+                    pilha.empilhar(tweet);
+                }
+                i++;
+            }
+
+            for (int j = DATABASE_LENGTH - 1; j >= 0; j--) {
+                database[j] = pilha.desempilhar();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Erro ao extrair dados.");
+            e.printStackTrace();
+        }
+
+        return database;
+    }
     
 }
